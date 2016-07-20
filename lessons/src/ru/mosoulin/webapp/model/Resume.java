24bf17e14658c80e11b2e2ebabbcf8b1905b062d
mosoulin <mosoulin@mail.ru>
2016-07-20 18:20:46 +0400

    add resume construction

--- lessons/src/ru/mosoulin/webapp/model/Resume.java
+++ lessons/src/ru/mosoulin/webapp/model/Resume.java
@@ -1,6 +1,8 @@
 package ru.mosoulin.webapp.model;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Admin on 12.07.16.
@@ -10,12 +12,50 @@ public class Resume {
     private String fullName;
     private String location;
     private String homePage;
-    private List<Contact> contacts;
-    private List<Section> sections;
+    private List<Contact> contacts=new LinkedList<>();
+    private List<Section> sections= new LinkedList<>();
 
     public Resume(String fullName, String location) {
+        this(UUID.randomUUID().toString(),fullName,location);
+
+    }
+
+    public Resume(String uuid, String fullName, String location) {
+        this.uuid = uuid;
         this.fullName = fullName;
         this.location = location;
+    }
+
+    public void addSection(Section section) {
+        sections.add(section);
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getFullName() {
+        return fullName;
     }
 
     @Override
@@ -26,6 +66,10 @@ public class Resume {
         Resume resume = (Resume) o;
 
         return uuid.equals(resume.uuid);
+
+    }
+
+    public Resume(String uuid) {
 
     }
 
